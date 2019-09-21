c87188f7c9f106f24e46e68d96b84a3bb4ab1ca3
suresh menon <suresh.naloor@gmail.com>
2019-09-21 19:02:55 +0300

    mongodb conn through mongoose

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '6319081623-p0uil9evccg08ssok3dhs6m9aiv5f8cl.apps.googleusercontent.com',
-    googleClientSecret: 'Oy7Osy_a3lR0IwhARxsGNxAn'
+    googleClientSecret: 'Oy7Osy_a3lR0IwhARxsGNxAn',
+    mongoURI: 'mongodb+srv://sureshunnimenon:ironhair394@penshopee-jtg4f.mongodb.net/test?retryWrites=true&w=majority'
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User')  // order is important this should come before passport import
+
 require('./helpers/passport')
+
+
+
+mongoose.connect(keys.mongoURI);
 
 const authRoutes = require('./routes/authRoutes')
 
